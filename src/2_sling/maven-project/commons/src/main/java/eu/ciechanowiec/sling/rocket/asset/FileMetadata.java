86fe4493d612b871bfd29de3bd6f6ad82279726e
Herman Ciechanowiec <herman@ciechanowiec.eu>
2024-10-20 19:43:42 +0200

    Add future file

--- src/2_sling/maven-project/commons/src/main/java/eu/ciechanowiec/sling/rocket/asset/FileMetadata.java
+++ src/2_sling/maven-project/commons/src/main/java/eu/ciechanowiec/sling/rocket/asset/FileMetadata.java
@@ -31,7 +31,18 @@ public class FileMetadata implements AssetMetadata {
      */
     @SuppressWarnings("WeakerAccess")
     public FileMetadata(File file) {
+        this(() -> file);
+    }
+
+    /**
+     * Constructs an instance of this class.
+     * @param fileSupplier {@link Supplier} that will provide a {@link File} for which
+     *                     this {@link FileMetadata} will be generated
+     */
+    @SuppressWarnings("WeakerAccess")
+    public FileMetadata(Supplier<File> fileSupplier) {
         mimeTypeSupplier = SneakySupplier.sneaky(() -> {
+            File file = fileSupplier.get();
             Tika tika = new Tika();
             log.trace("Detecting the mime type of {}", file);
             String detectedMimeType = tika.detect(file);
@@ -40,7 +51,7 @@ public class FileMetadata implements AssetMetadata {
         });
         allSupplier = () -> Map.of(PN_MIME_TYPE, mimeType());
         propertiesSupplier = Optional::empty;
-        log.trace("Initialized {} out of this file: {}", this, file);
+        log.trace("Initialized {}", this);
     }
 
     @Override
