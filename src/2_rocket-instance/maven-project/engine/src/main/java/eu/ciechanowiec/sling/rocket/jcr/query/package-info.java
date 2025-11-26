35710f0d372101d370196d2a42c48e209515d5e8
Herman Ciechanowiec <herman@ciechanowiec.eu>
2025-11-26 10:22:31 +0100

    Misc refinements

---
+++ src/2_rocket-instance/maven-project/engine/src/main/java/eu/ciechanowiec/sling/rocket/jcr/query/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Utilities for {@link javax.jcr.query.Query}.
+ */
+@Export
+@Version("1.0.0")
+package eu.ciechanowiec.sling.rocket.jcr.query;
+
+import org.osgi.annotation.bundle.Export;
+import org.osgi.annotation.versioning.Version;
